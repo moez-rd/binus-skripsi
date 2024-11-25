63843c9f2d4ecd21af67a7d6df9b7fc469104643
Rahmat <slow9ie@gmail.com>
2024-11-25 15:14:50 +0700

    'Update

--- lib/firebase/queries.ts
+++ lib/firebase/queries.ts
@@ -14,22 +14,18 @@ export async function insertThesis(thesis: ThesisProps) {
 }
 
 export async function searchThesis(str: string) {
-  const citiesRef = collection(firestore, "theses");
-
-  const q = query(citiesRef, where("title", ">=", str));
-
-  const querySnapshot = await getDocs(q);
-  const theses: ThesisProps[] = querySnapshot.docs.map((doc) => {
-    return {
-      title: doc.data()["title"],
-      studentId: doc.data()["studentName"],
-      studentName: doc.data()["studentName"],
-      year: doc.data()["year"],
-      abstract: doc.data()["abstract"],
-    } as ThesisProps;
-  });
-
-  console.log(theses);
-
-  return theses;
+  // const citiesRef = collection(firestore, "theses");
+  // const q = query(citiesRef, where("title", ">=", str));
+  // const querySnapshot = await getDocs(q);
+  // const theses: ThesisProps[] = querySnapshot.docs.map((doc) => {
+  //   return {
+  //     title: doc.data()["title"],
+  //     studentId: doc.data()["studentName"],
+  //     studentName: doc.data()["studentName"],
+  //     year: doc.data()["year"],
+  //     abstract: doc.data()["abstract"],
+  //   } as ThesisProps;
+  // });
+  // console.log(theses);
+  // return theses;
 }
